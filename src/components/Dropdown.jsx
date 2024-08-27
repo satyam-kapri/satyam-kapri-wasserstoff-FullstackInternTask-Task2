a192aabb438060629b9e1112eb8be1ec96bc2731
satyam-kapri <ksatyamk8@gmail.com>
2024-08-27 11:22:44 +0530

    successfull vercel deployment

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,6 +4,7 @@ function Dropdown({item2,item3,selectItem,setShowDropdown}) {
   return (
     
     <div class="absolute top-16 z-20 flex flex-col rounded-lg w-full max-w-36 p-2 bg-gray-950">
+      
                          {/* menu items of dropdown */}
                         <div class="menu-item  hover:bg-zinc-900 cursor-pointer rounded-lg" onClick={()=>{selectItem(item2);setShowDropdown(false);}}>
                                 <span class="menu-title ml-2 ">
