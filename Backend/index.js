13db0d8c1ff4cada5596dddebf689e53b57fffc8
satyam-kapri <ksatyamk8@gmail.com>
2024-08-27 10:27:35 +0530

    changing backend code

--- Backend/index.js
+++ Backend/index.js
@@ -7,28 +7,40 @@ dotenv.config();
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000;
-const uri = process.env.MONGODB_URI;
 
-// connect to mongodb
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch((err) => console.error('MongoDB connection error:', err));
+let isConnected;
+
+const connectToDatabase = async () => {
+  if (isConnected) {
+    return;
+  }
+  try {
+    const uri = process.env.MONGODB_URI;
+    const connection = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = connection.connections[0].readyState;
+    console.log('Connected to MongoDB Atlas');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    throw err;
+  }
+};
 
 // Route to get all documents from the collection
 app.get('/getjsondata', async (req, res) => {
   try {
+    await connectToDatabase();
     const db = mongoose.connection.db;
-    const collection = db.collection('JsonData'); 
+    const collection = db.collection('JsonData');
     const documents = await collection.find({}, { projection: { _id: 0 } }).toArray(); // Get all documents
-    res.status(200).json(documents); 
+    res.status(200).json(documents);
   } catch (err) {
     res.status(500).json({ message: 'Error fetching documents', error: err.message });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on ${port} port`);
 });
